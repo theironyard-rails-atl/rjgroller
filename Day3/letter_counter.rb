08080dee59aa595428f8fa3b6ac381a60285a9f2
Robert Groller <robertgroller@gmail.com>
2014-07-30 20:32:01 -0400

    added letter_counter

---
+++ Day3/letter_counter.rb
@@ -0,0 +1,28 @@
+class LetterCounter
+
+  def initialize(words)
+    @letters = words.downcase.chars
+    @count_of_letters = Hash.new
+  end
+
+  def letters_to_hash
+    @letters.each { |letter| @count_of_letters[letter] = 0 }
+  end
+
+  def letter_count_to_hash
+    @count_of_letters.each { |key, value| @count_of_letters[key] = @letters.count(key) }
+  end
+
+  def print_letter_counts
+    @count_of_letters.each { |key, value| puts "#{key}: #{value}"}
+  end
+
+  def print_specific(input)
+    if @count_of_letters.has_key?(input)
+      puts "#{input}: #{@count_of_letters[input]}"
+    else
+      "That letter does not occur"
+    end
+  end
+
+end
---
+++ Day3/letter_script.rb
@@ -0,0 +1,19 @@
+require './letter_counter.rb'
+
+puts "Please provide a string: "
+test_string = gets.chomp
+
+testrun = LetterCounter.new(test_string)
+testrun.letters_to_hash
+testrun.letter_count_to_hash
+testrun.print_letter_counts
+
+puts "Count for a specific letter? (y/n)"
+answer = gets.chomp[0].downcase
+if answer == "y"
+  puts "Enter letter: "
+  letter = gets.chomp[0].downcase
+  testrun.print_specific(letter)
+lelse
+  exit
+end
